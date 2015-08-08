5bfc9880117350de6b7bc04f729d832c0d98b92a
chucklehound <chris@fizzylightdesign.com>
2015-08-08 19:57:52 +0100

    Setting things up

---
+++ js/soundzee.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var buttons = document.querySelectorAll('button#soundzee');
+
+    for (var i = 0, max = buttons.length; i < max; i++) {
+        buttons[i].addEventListener('click', addPressedClass);
+    }
+
+    function addPressedClass(e) {
+        var btn = e.target;
+        btn.classList.add('pressed');
+        setTimeout(function () {
+            btn.classList.remove('pressed');
+        }, 400);
+    }
+}, false);
